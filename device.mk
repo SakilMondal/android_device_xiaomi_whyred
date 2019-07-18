50f0da3fff87cdc664e86c3e4da3c30c3cfd6e91
SakilMondal <mondalsakil@gmail.com>
2019-07-18 16:33:23 +0000

    whyred: build SnapdragonCamera2
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap
+    SnapdragonCamera2
 
 # CNE
 PRODUCT_PACKAGES += \
