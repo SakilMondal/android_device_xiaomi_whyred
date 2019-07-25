6c754b8602b9327ea8111d66b9670532a477030a
SakilMondal <mondalsakil@gmail.com>
2019-07-25 04:19:52 +0000

    whyred: Build FM2

--- device.mk
+++ device.mk
@@ -197,6 +197,9 @@ PRODUCT_PACKAGES += \
     libqcomfm_jni \
     qcom.fmradio
 
+PRODUCT_BOOT_JARS += \
+    qcom.fmradio
+
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
