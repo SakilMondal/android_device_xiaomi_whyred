6ebc3af2e05e22a7b79ec3d3cf60191a33d087bc
Sakil_Mondal <mondalsakil@gmail.com>
2019-09-17 17:32:02 +0000

    whyred: Add Mi ringtone and notification tone
    Signed-off-by: Sakil_Mondal <mondalsakil@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Mi Ringtone
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/Mi.ogg:system/media/audio/ringtones/Mi.ogg
+
+# Mi Notification
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/Fresh.ogg:system/media/audio/notifications/Fresh.ogg
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
