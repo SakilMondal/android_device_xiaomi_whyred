53d3abf88cd6a2de8b2ad8f3b25bded347522ad0
SakilMondal <mondalsakil@gmail.com>
2020-03-27 13:36:16 +0530

    whyred: Remove LineageOS Stuffs (2/2)
    Revert "whyred: Exclude Snap overlays from RRO"
    
    This reverts commit 16a4a4ed24d7bd8717f86b90f1f23eec04969813.
    
    Revert "whyred: Exclude lineage-sdk overlays from RRO"
    
    This reverts commit 945d554438a2e0d2dec65f2ffc641c3f8d3b5e2d.
    
    Revert "whyred: Enforce RRO for all resource overlays"
    
    This reverts commit a56856e832cd691625a84c2aa2c42b9601ce9457.
    
    whyred: DO NOT ENFORCE_RRO_TARGETS
    
    whyred: Drop Trust HAL
    
    whyred: Drop LiveDisplay HAL
    
    whyred: Remove Lineage Power hidl from manifest
    
    whyred: Move Overlays and Remove Lineage leftovers
    
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- device.mk
+++ device.mk
@@ -28,13 +28,7 @@ $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/xiaomi/whyred
@@ -322,10 +316,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_whyred
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
@@ -458,10 +448,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
