9bb6568d544b959574a039c9deb40723d8ecea67
SakilMondal <mondalsakil@gmail.com>
2020-07-07 12:48:48 +0530

    whyred: Update Build Fingerprint from Coral QQ3A.200705.002
    Change-Id: I27c8abaf4e57983ea99f7478daf34c4c5d34bd66
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -45,8 +45,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200501.001.B2 6352890 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
 
 #Build FP to be picked by both system and vendor
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
-
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
