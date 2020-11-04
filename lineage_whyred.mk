c770ec290c81a30e7fd62b354490205553d026e8
SakilMondal <mondalsakil@gmail.com>
2020-11-04 13:18:08 +0530

    whyred: Update Build Fingerprint from Coral RP1A.201105.002
    Change-Id: Ia970517f651562cb5dff931f285a20ea8a66a4ce
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -39,6 +39,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201105.002 6869500 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
