ad5404cf85bc5daddd442effa2bfca843c715afe
SakilMondal <mondalsakil@gmail.com>
2020-11-08 12:50:41 +0530

    whyred: Enable ARcore
    Change-Id: I817df30bc6b934874f130bb42357d078aa347a59

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -27,6 +27,7 @@ TARGET_INCLUDE_WIFI_EXT := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
