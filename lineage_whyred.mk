9f00218537a053d75473169ee05fce66eba5ef01
SakilMondal <mondalsakil@gmail.com>
2021-07-08 11:55:54 +0530

    whyred: Update Build Fingerprint from Redfin RQ3A.210705.001

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -39,6 +39,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
